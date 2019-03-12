8aec85e9e6daf0f29abb20f24f50a7d6583be114
Robin Louarn <rlouarn@MacBook-Pro-de-Robin-2.local>
2019-03-12 09:16:37 +0100

    junit

---
+++ src/test/java/com/iup/tp/twitup/ihm/TwitupMockTest.java
@@ -0,0 +1,50 @@
+package com.iup.tp.twitup.ihm;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwitupMockTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void showGUI() {
+    }
+
+    @Test
+    void initGUI() {
+    }
+
+    @Test
+    void addUserInDatabase() {
+    }
+
+    @Test
+    void sendUser() {
+    }
+
+    @Test
+    void generateUser() {
+    }
+
+    @Test
+    void addTwitInDatabase() {
+    }
+
+    @Test
+    void sendTwit() {
+    }
+
+    @Test
+    void generateTwit() {
+    }
+}
